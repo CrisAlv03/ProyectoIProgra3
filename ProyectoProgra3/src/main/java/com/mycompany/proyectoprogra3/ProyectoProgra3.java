a8fdedae58c706e09dd78f803b1d72e226cf1b30
CrisAlv03 <91854160+CrisAlv03@users.noreply.github.com>
2023-03-21 00:07:10 -0600

    Add files via upload

---
+++ ProyectoProgra3/src/main/java/com/mycompany/proyectoprogra3/ProyectoProgra3.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.proyectoprogra3;
+
+/**
+ *
+ * @author CrisA
+ */
+public class ProyectoProgra3 {
+
+    public static void main(String[] args) {
+        new PilArbolExp().setVisible(true);
+    }
+}
